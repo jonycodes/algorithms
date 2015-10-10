ea337129923feac0db347fd020a916c1f387de56
jonycodes <mr.joannier@yahoo.com>
2015-10-09 20:17:52 -0400

    updated ;

--- hacker/src/hacker/Hacker.java
+++ hacker/src/hacker/Hacker.java
@@ -6,9 +6,7 @@
 package hacker;
 
 import static java.lang.StrictMath.abs;
-
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -22,91 +20,54 @@ public class Hacker {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //test(line());
-        plusMinus();
+
+        test("Matrix sum is"+ matrixSum());
+        //plusMinus();
         //test(factorial(scan()));
     }
 
 
-    static int scan() {
-        Scanner in = new Scanner(System.in);
-        return in.nextInt();
-    }
-
-    static long scan(boolean s) {
-        Scanner in = new Scanner(System.in);
-        return in.nextLong();
-    }
-
-    static String line() {
-        Scanner in = new Scanner(System.in);
-        return in.nextLine();
-    }
-
-    //Diagonal Difference (not complete)
-
-    static int[] getInt(int i, String s) { //gets input and concatinate them 
-        String value = "";
-        int[] array = new int[i];
-        int counter = 0;
-        for (int j = 0; j <= s.length(); j++) {
-            test(j);
-            if (j < s.length() && s.charAt(j) != ' ') {
-                if (s.charAt(j + 1) == ' ') {
-                    array[counter] = Integer.parseInt(String.valueOf(s.charAt(j)));;
-                    //test(Integer.parseInt(String.valueOf(s.charAt(j))));
-                    counter++;
-                } else {
-                    array[counter] = Integer.parseInt(String.valueOf(s.charAt(j) + "" + s.charAt(j + 1)));
-                    //test(Integer.parseInt(String.valueOf(s.charAt(j) + "" + s.charAt(j + 1))));
-                    j++;
-                    counter++;
-                }
-            }
-
-        }
-
-        return array;
-    }
-    
-    static int[] copyArray(int pointer,int[] refarray, int[]array){
-        int counter = 0;
-        while (counter < pointer) {  //copy two arrays together
-            int ref = 0;
-            test(line());
-            refarray = getInt(pointer, line());
-            System.arraycopy(refarray,0,array, ref, pointer*counter+1);
-            counter++;
-        }
-        return array;
-    }
+    //Diagonal Difference of one NxN matrix
 
     public static int matrixSum() {
-        int pointer = scan();
         int counter = 0;
-
         int sum = 0;
         int sum1 = 0;
-        int[] refarray = new int[pointer]; //refarray to be copied
-        int[] array = new int[pointer]; //final array
-        copyArray(pointer, refarray, array); //copy two arrays
-        
+        Scanner in = new Scanner(System.in);
+        int pointer = in.nextInt();
+        in.nextLine();
+        String s = "";
+        int[] array = new int[pointer*pointer]; //final array
+        while(counter < pointer){
+            s += in.nextLine()+" ";
+            counter++;
+        }
+
+        counter =0; // reset counter
+        int counter1 = 0; // start second counter
+        while(counter < pointer*pointer) {
+            String[] copied = getValue(s, counter1);
+            int number = Integer.parseInt(copied[0]);
+            counter1 = Integer.parseInt(copied[1]);
+            array[counter] = number;
+            //test(array[counter]);
+            counter++;
+        }
+
         int pos = 0; // position in the matrix
-        counter = 1; // number of sums
-        int jump = pointer + 1;  //next position jump
-        while (counter <= pointer) {  // Loop through the array
-            //test(pos);
+        counter = 0; // Reset counter
+        int jump = pointer + 1;  //next diagonal position
+        while (counter < pointer) {  // Loop through the array
             sum += array[pos];
             pos += jump;
             counter++;
             //test(sum);
         }
 
-        pos = pointer - 1;    //position in the opposite matrix
-        jump = pointer - 1; //next position jump
-        counter = 1;       //counter number of opperations
-        while (counter <= pointer) {   //loop through the aarray
-            //test(pos);
+        pos = pointer - 1;  //advanced backwards position in the matrix
+        jump = pointer - 1; //next diagonal position
+        counter = 0;        //Resets counter
+        while (counter < pointer) {   //loop through the aarray
             sum1 += array[pos];
             pos += jump;
             counter++;
@@ -115,31 +76,73 @@ public class Hacker {
         return abs(sum1 - sum); //returns absolute value
     }
 
+
+    //Returns individual value from a String in an array at pos(0) and returns pointer in the array after the value at pos(1)
+    static String[] getValue(String s, int counter){
+        String[] array = new String[2];
+        String value = "";
+        if(s.charAt(counter) == ' '){
+            counter++;
+        }
+       //test("counter is " +counter);
+        while(s.charAt(counter) != ' '){
+           value += s.charAt(counter);
+            counter++;
+       }
+        //test("Counter on get value "+counter);
+        array[0] = value;
+        array[1] = String.valueOf(counter);
+        return array;
+    }
+
+
     //Plus minus
 
     static void plusMinus(){
-        int pointer = scan();
+        Scanner in = new Scanner(System.in);
+        float pointer = in.nextInt();
         int counter = 0;
-        int positives =0;
-        int negatives=0;
-        int zeros = 0;
-        while(counter < pointer){
-            int number = scan();
-            if(number>0){
-                positives++;}
-            if(number==0){
-                zeros++;
-            }
-            else if(number<0){
-                negatives++;
-            }
-            counter++;
+        double positives =0;
+        double negatives=0;
+        double zeros = 0.000;
+        in.nextLine();
+        String s = in.nextLine()+" ";
+        test(s);
+        String[] array;
+        int number;
+        int counter1 = 0;
+        while(counter1 < pointer) {
+
+            //try {
+                array = getValue(s, counter);
+                number = Integer.parseInt(array[0]);
+                counter = Integer.parseInt(array[1]);
+                test("number "+number);
+                //test("counter "+counter);
+                if (number > 0) {
+                    positives++;
+                }
+                if (number == 0) {
+                    zeros++;
+                } else if (number < 0) {
+                    negatives++;
+                }
+            counter1++;
+            /*} catch (IndexOutOfBoundsException e) {
+                test("IndexOutofBound");
+                break;
+            }*/
+
         }
-        double b = pointer;
-         test("positives "+positives/b +" Negatives "+negatives/b+" Zeros "+zeros/b);
+            float b = pointer;
+        System.out.println();
+        System.out.format("%.3f%n",positives/b);
+        System.out.format("%.3f%n",negatives/b);
+        System.out.format("%.3f%n",zeros/b);
     }
 
-//Extra long Factorial
+
+//Extra long Factorial returns N!
 
     static BigInteger factorial(int n){
        BigInteger factorial = new BigInteger(String.valueOf(1));
@@ -154,6 +157,16 @@ public class Hacker {
         return factorial;
     }
 
+    static double scanDouble() {
+        Scanner in = new Scanner(System.in);
+        return in.nextDouble();
+    }
+
+    static int scan(){
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+
     static void test(int s) {
         System.out.println("Test value: " + s);
     }
@@ -162,7 +175,7 @@ public class Hacker {
         System.out.println("Test value: " + s);
     }
 
-    static void test(ArrayList s) {
+    static void test(int[] s) {
         System.out.println("Test value: " + s);
     }
 
